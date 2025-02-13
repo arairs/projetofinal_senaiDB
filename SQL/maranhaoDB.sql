13e0dabc561be9fe8e21d9e4c6b91472328e3b21
Thiagosilva <thiagoantonio2108@gmail.com>
2025-02-12 21:45:09 -0300

    update db

--- SQL/maranhaoDB.sql
+++ SQL/maranhaoDB.sql
@@ -1,28 +1,34 @@
-create schema if not exists churrascoMaranhao default character set utf8;
-use churrascoMaranhao;
+-- create schema if not exists churrascoMaranhao default character set utf8;
+create database churrascoMaranhao;
 
-create table if not exists comanda(
-	idComanda int auto_increment,
-    mesa int not null,
-    clienteNome varchar(255),
-    abertura datetime not null,
-    fechamento datetime not null,
-		constraint PK_comanda primary key(idComanda)
-);
+-- use churrascoMaranhao;
 
-create table if not exists cardapio(
+create table if not exists produto(
+	idProduto serial,
 	nome varchar(150) not null,
     descrição varchar(255) not null,
     preco decimal(5,2) not null,
-    qtdEstoque int
+    qtdEstoque int,
+    	constraint PK_produto primary key(idProduto)
+);
+
+create table if not exists comanda(
+	idComanda serial,
+    mesa int not null,
+    clienteNome varchar(255),
+    abertura timestamp not null,
+    fechamento timestamp not null,
+		constraint PK_comanda primary key(idComanda)
 );
 
 create table if not exists pedido(
-	idPedido int auto_increment,
+	idPedido serial,
     idComanda int,
-    dataPedido datetime not null,
+    idProduto int,
+    dataPedido timestamp not null,
     nomePedido varchar(150) not null,
     qtdPedido int not null,
 		constraint PK_pedido primary key(idPedido),
-        constraint FK_pedido_comanda foreign key(idComanda) references comanda(idComanda)
+        constraint FK_pedido_comanda foreign key(idComanda) references comanda(idComanda),
+        constraint FK_pedido_produto foreign key(idProduto) references produto(idProduto)
 );
