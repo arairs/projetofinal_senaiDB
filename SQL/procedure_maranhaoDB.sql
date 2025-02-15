b6dd8a9d437c82196dfef809e4f24363a93f5055
felipe soares siqueira <felipesoaressiqueira@gmail.com>
2025-02-14 21:17:47 -0300

    criando procedure

--- SQL/procedure_maranhaoDB.sql
+++ SQL/procedure_maranhaoDB.sql
@@ -1,13 +1,17 @@
 
-
 create procedure recarga_produto(qt int, id int)
 LANGUAGE plpgsql
 AS $$
 begin
 	
-	update produto 	set qtdEstoque = qt	where idProduto = id;
+	update produto set qtdEstoque = qt + qtdEstoque	where idProduto = id;
 	
 end;
 $$
 
+drop procedure recarga_produto;
 
+select idproduto, nome, qtdestoque from produto p 
+order by idproduto ;
+
+call recarga_produto(20,2);
